25c8f16c740c9cd0367f76b3066fbd2032a209e3
Sam Niyazi <sam2003@niyazi.com>
2019-02-05 14:17:08 -0600

    Decode html in devsite index. Revised copyright related code.

---
+++ DevSitesIndex/Util/Odds.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// 01/28/2019 04:56 pm - SSN
+
+namespace DevSitesIndex.Util
+{
+    public class Odds
+    {
+
+        public static HtmlString showCopyright()
+        {
+            return new HtmlString("<p>&copy; 2018 - dev.testsam.com</p>");
+        }
+    }
+}
