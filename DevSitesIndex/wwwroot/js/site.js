383ab471afd92f11ace6e96cf564d546d10fdf9e
Sam Niyazi <sam2003@niyazi.com>
2019-03-14 11:04:55 -0500

    indexNowrapCol and more styling.

--- DevSitesIndex/wwwroot/js/site.js
+++ DevSitesIndex/wwwroot/js/site.js
@@ -9,8 +9,9 @@ $(function () {
 
         var cmdName = $(this).attr('cmd-name');
         var popupName = $(this).attr('popup-name');
+        var jQueryObjectName = $(this).attr('jQueryObjectName');
 
-        alert('clicked me! cmdName [' + cmdName + "] popupName  [" + popupName + "]");
+        //        alert('clicked me! cmdName [' + cmdName + "] popup-name  [" + popupName + "]");
 
         if (cmdName === "open-popup") {
             $(popupName).modal({ backdrop: 'static', keyboard: false });
@@ -18,7 +19,44 @@ $(function () {
             $("#addSite_PageContent").load("/times/start");
         }
 
-        
+        // 03/14/2019 09:33 am - SSN - Adding hide and show
+        if (cmdName === "hideObject") {
+
+            $(jQueryObjectName).hide();
+
+        }
+
+        if (cmdName === "showObject") {
+
+            $(jQueryObjectName).show();
+
+        }
+
+        if (cmdName === "smooth-scroll") {
+            //$('body').scrollspy({ target: jQueryObjectName });
+
+            document.querySelector(jQueryObjectName).scrollIntoView({
+                behavior: 'smooth'
+            });
+
+        }
 
     });
+
+
+
+    // 03/14/2019 10:28 am - SSN
+
+    $(window).on('scroll', function () {
+        var y = $(window).scrollTop();
+
+        if (y > 0) {
+            $('.fixed_anchor').fadeIn('slow');
+        } else {
+            $('.fixed_anchor').fadeOut('slow');
+        }
+    });
+
+
+
 });
