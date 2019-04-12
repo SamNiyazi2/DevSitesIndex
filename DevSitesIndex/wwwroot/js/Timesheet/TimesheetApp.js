98a67947310e8a0aa17bd2553601265dfe9b2ee4
Sam Niyazi <sam2003@niyazi.com>
2019-04-12 06:00:40 -0500

    Added initial process to call timesheet from jobs index.

---
+++ DevSitesIndex/wwwroot/js/Timesheet/TimesheetApp.js
@@ -0,0 +1,42 @@
+﻿
+// 04/12/2019 01:42 am - SSN - [20190412-0142] - TimesheetApp
+
+var timesheetApp = angular.module('timesheetApp', ['ngRoute',  'ui.bootstrap']);
+
+console.log('TimesheetApp.js');
+
+
+//timesheetApp .config(function ($routeProvider) {
+
+//    $routeProvider
+//        .when('/showCreateEmployeeForm_TEMP', {
+//            templateUrl: 'app/EmployeeForm/efTemplate.html',
+//            controller: 'timesheetController'
+//        })
+//        .otherwise({
+//            redirectTo: '/home'
+//        });
+
+
+//});
+
+
+
+timesheetApp.controller("timesheetController", function ($scope, $modal) {
+
+    console.log('TimesheetApp.js - timesheetController');
+
+ 
+    $scope.showCreateEmployeeForm = function () {
+          
+        $modal.open({
+            templateUrl: 'js/timesheet/timesheetTemplate.html',
+            controller: 'TimesheetController',
+            windowClass: 'ssn-mobile-modal'
+        }); 
+
+    };
+});
+
+//alert('timsheetapp.js loaded');
+console.log('TimesheetApp.js  - bottom');
---
+++ DevSitesIndex/wwwroot/js/Timesheet/TimesheetController.js
@@ -0,0 +1,54 @@
+﻿// 04/12/2019 03:57 am - SSN - [20190412-0142] - TimesheetApp
+
+
+timesheetApp.controller('TimesheetController',
+    function efController($scope, $window, $routeParams, $modalInstance) {
+
+
+        if ($routeParams.id) {
+            $scope.employee = DataService.getEmployee($routeParams.id);
+        }
+        else {
+            $scope.employee = { id: 0 };
+        }
+
+
+
+        //$scope.editableEmployee = angular.copy($scope.employee);
+
+
+
+        $scope.departments = [
+            "Engineering",
+            "Marketing",
+            "Finance",
+            "Administration"
+        ];
+
+
+
+        $scope.submitForm = function () {
+
+        
+            if ($scope.editableEmployee.id == 0) {
+                DataService.insertEmployee($scope.editableEmployee);
+            }
+            else {
+                DataService.updateEmployee($scope.editableEmployee);
+            }
+
+            $scope.employee = angular.copy($scope.editableEmployee);
+
+            $modalInstance.close();
+        };
+
+
+
+        $scope.cancelForm = function () {
+
+
+            $modalInstance.dismiss(); //same as cancel???
+
+        };
+
+    })
