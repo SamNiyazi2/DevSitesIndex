37686d83c680baa12337c9ae8a901412f238053a
Sam Niyazi <sam2003@niyazi.com>
2019-08-12 19:58:04 -0500

    No migration trigger for the new identity context. Updated.

--- DevSitesIndex/Areas/Identity/Data/DevSitesIndexIdentityDbContext.cs
+++ DevSitesIndex/Areas/Identity/Data/DevSitesIndexIdentityDbContext.cs
@@ -5,14 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DevSitesIndex.Areas.Identity.Data
 {
     public class DevSitesIndexIdentityDbContext : IdentityDbContext<IdentityUser>
     {
-        public DevSitesIndexIdentityDbContext(DbContextOptions<DevSitesIndexIdentityDbContext> options)
+
+        // 08/12/2019 07:54 pm - SSN - [20190812-0945] - [019] - Add identity
+        // IConfiguration configuration & do_database_Migration
+        public DevSitesIndexIdentityDbContext(DbContextOptions<DevSitesIndexIdentityDbContext> options, IConfiguration configuration)
             : base(options)
         {
+
+
+
+            bool do_database_Migration = false;
+            bool.TryParse(configuration["Database_Migration"], out do_database_Migration);
+
+
+            if (do_database_Migration)
+            {
+                try
+                {
+                    Database.SetCommandTimeout(6000);
+                    Database.Migrate();
+                }
+                catch (Exception)
+                {
+                    // Do nothing here.
+                }
+            }
+
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -21,6 +45,9 @@ namespace DevSitesIndex.Areas.Identity.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+            
         }
+
+
     }
 }
