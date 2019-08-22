fd5a5e3188ef4cbc1cf02fe5e597678fdb0a5757
Sam Niyazi <sam2003@niyazi.com>
2019-08-22 16:46:40 -0500

    Debug production databsae migration failure.

---
+++ DevSitesIndex/Migrations/20190822214318_Test-Migration-20190822a.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevSitesIndex.Migrations
+{
+    public partial class TestMigration20190822a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
