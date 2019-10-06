1cb422828b3d70d9e88dd2b01c983d86a19d658e
Sam Niyazi <sam2003@niyazi.com>
2019-10-06 15:13:02 -0500

    1001_007_Dropdown_Pending... 1010-001-Project-search

--- DevSitesIndex/timesheetsSupport7/dist/out-tsc/src/app/util/timelog-route-activator.service.js
+++ DevSitesIndex/timesheetsSupport7/dist/out-tsc/src/app/util/timelog-route-activator.service.js
@@ -3,17 +3,28 @@ import * as tslib_1 from "tslib";
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../shared/data.service';
+console.log('timelogroute-activator.service - 20191006-1343');
 var TimelogRouteActivatorService = /** @class */ (function () {
     function TimelogRouteActivatorService(dataService, route) {
         this.dataService = dataService;
         this.route = route;
+        console.log('timelogroute-activator.service - 20191006-1344 - constructor ');
     }
     TimelogRouteActivatorService.prototype.canActivate = function (route) {
-        var recordExists = !!this.dataService.getTimesheet(route.params['id']);
-        if (!recordExists) {
-            this.route.navigate(['e404']);
-        }
-        return recordExists;
+        var _this = this;
+        console.log('timelogroute-activator.service - 20191006-1345 - canActivate ');
+        var id = +this.dataService.getTimesheet(route.params['id']);
+        var result = this.dataService.getTimesheet(route.params['id']).toPromise().then(function (r) {
+            var recordExists = !!r;
+            console.log(r);
+            console.log("id [" + id + "] recordexists [" + recordExists + "]");
+            return recordExists;
+        }, function (r) {
+            _this.route.navigate(['e404']);
+            return false;
+        });
+        console.log("result  [" + result + "]");
+        return result;
     };
     TimelogRouteActivatorService = tslib_1.__decorate([
         Injectable({
