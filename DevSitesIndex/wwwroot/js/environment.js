ee60418f6a298ba06c3a25b10ed4dc1abf325ca8
Sam Niyazi <sam2003@niyazi.com>
2019-10-10 17:15:34 -0500

    Merge pending branches - master stashes.

--- DevSitesIndex/wwwroot/js/environment.js
+++ DevSitesIndex/wwwroot/js/environment.js
@@ -1,16 +1,9 @@
-﻿
 // 10/01/2019 09:47 am - SSN - [20191001-0944] - [003] - Adding Application Insights for JavaScript
-
 // https://devblogs.microsoft.com/premier-developer/add-application-insights-to-an-angular-spa/
-
-export const environment = {
-
-
+export var environment = {
     appInsights: {
-
         // instrumentationKey: 'd9f2121b-9475-4fd6-97cb-4df016e33ce3'
         instrumentationKey: ''
-
     }
-
 };
+//# sourceMappingURL=environment.js.map
