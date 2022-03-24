8512813736bfd8f249975b0acda52f22ce67176e
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-03-24 08:45:15 -0500

    Added code to generate image from text.

---
+++ DevSitesIndex/Controllers/TextToImageAPIController.cs
@@ -0,0 +1,102 @@
+﻿using DevSitesIndex.Entities;
+using Microsoft.AspNetCore.Mvc;
+using SSN_GenUtil_StandardLib;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+// 03/24/2022 06:35 am - SSN 
+
+namespace DevSitesIndex.Controllers
+{
+    [Route("toi")]
+    [ApiController]
+    public class TextToImageAPIController : ControllerBase
+    {
+        private readonly DevSitesIndexContext context;
+        private readonly ILogger_SSN logge;
+
+        public TextToImageAPIController(DevSitesIndexContext context, ILogger_SSN logge)
+        {
+            this.context = context;
+            this.logge = logge;
+        }
+
+
+        class DicRec
+        {
+            public MemoryStream ms_ { get; set; }
+            public DateTime posted { get; set; } = DateTime.Now;
+
+            public bool Expired
+            {
+                get
+                {
+                    return DateTime.Now > posted.AddSeconds(60);
+                }
+            }
+        }
+
+        static ConcurrentDictionary<string, DicRec> dic = new System.Collections.Concurrent.ConcurrentDictionary<string, DicRec>();
+
+        // GET: api/<TextToImageAPIController>
+        [HttpGet("jobs/{id}/TotalHours")]
+        public FileContentResult Get(string id)
+        {
+
+            MemoryStream ms = null;
+            string contentType = "text/html";
+
+            id = (id ?? "").Trim();
+
+            dic.TryGetValue(id, out DicRec dicRec);
+
+
+            if (dicRec == null || dicRec.Expired)
+            {
+                ms = new MemoryStream();
+
+                if (int.TryParse(id, out int jobId))
+                {
+                    Job job = context.Jobs.Where(j => j.JobID == jobId).FirstOrDefault();
+
+                    if (job != null)
+                    {
+                        contentType = "image/bmp";
+                        SSN_TextToImageLib.Drawings.GenerateImageFromText util = new SSN_TextToImageLib.Drawings.GenerateImageFromText();
+                        ms = util.GenerateDrawingFromText_AsStream(string.Format("Timelog: {0}", job.JobTotalHours));
+                    }
+                }
+
+                if (dicRec == null)
+                {
+                    dicRec = new DicRec { ms_ = ms };
+                    dic.TryAdd(id, dicRec);
+                }
+                else
+                {
+                    dicRec.posted = DateTime.Now;
+                }
+
+            }
+            else
+            {
+                //ms = dicRec.ms_array;
+                contentType = "image/bmp";
+                ms = dicRec.ms_;
+            }
+
+
+            byte[] bytes = ms.ToArray();
+
+            return new FileContentResult(bytes, contentType);
+        }
+
+
+    }
+}
---
+++ SSN_TextToImageLib/Drawings/GenerateImageFromText.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 03/24/2022 06:30 am - SSN - Copied from C:\Sams_Projects\Training\WindowsFormsApp_Testing\Drawings
+
+namespace SSN_TextToImageLib.Drawings
+{
+    public partial class GenerateImageFromText
+    {
+
+
+
+        public MemoryStream GenerateDrawingFromText_AsStream(string text)
+        {
+            Image image = GenerateDrawingFromText_AsBMP(text);
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            return ms;
+        }
+
+        // https://stackoverflow.com/questions/2070365/how-to-generate-an-image-from-text-on-fly-at-runtime
+        public Image GenerateDrawingFromText_AsBMP(string text, Font font = null, Color backColor = default, Color textColor = default)
+        {
+            if (font == null)
+            {
+                font = new Font("Verdana", 10f);
+            }
+
+            if ( textColor == default)  // Cannot defalut parameter "Must be a compiler-time constance
+            {
+                textColor = Color.White;
+            }
+
+            Image image = new Bitmap(1, 1);
+
+            Graphics drawing = Graphics.FromImage(image);
+
+            SizeF textSize = drawing.MeasureString(text, font);
+
+            image.Dispose();
+            drawing.Dispose();
+
+            image = new Bitmap((int)textSize.Width, (int)textSize.Height);
+
+            drawing = Graphics.FromImage(image);
+
+            drawing.Clear(backColor);
+
+
+            Brush textBrush = new SolidBrush(textColor);
+
+
+            drawing.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            drawing.DrawString(text, font, textBrush, 0, 0);
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return image;
+
+        }
+        // ssn-todo
+
+        // https://stackoverflow.com/questions/6311545/c-sharp-write-text-on-bitmap
+
+    }
+}
