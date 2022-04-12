a19f103a2b57a988626e290b00ab58daa45322dd
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-04-12 02:58:11 -0500

    webpack.config.js was excluded for '.js'

---
+++ DevSitesIndex/ReactApp/webpack.config.js
@@ -0,0 +1,43 @@
+﻿
+// 04/12/2022 12:22 am - SSN 
+// https://www.mackolicious.com/blog/reactjs-into-aspnet-core
+
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+
+	devtool: "source-map",
+
+	resolve: {
+		extensions: [".ts", ".tsx"]
+	},
+	output: {
+		path: path.resolve(__dirname, "../wwwroot/ReactDist"),
+		filename: "[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader"
+					}
+				]
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
+			}
+		]
+	},
+
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM",
+		"react-router-dom": "ReactRouterDOM"
+	}
+};
