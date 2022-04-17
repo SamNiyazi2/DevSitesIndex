586963325c1d4ec553500b44496beb16ebbb0fd2
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-04-17 00:41:52 -0500

    Forced to upgrade to React 18.

--- DevSitesIndex/ReactApp/src/index.tsx
+++ DevSitesIndex/ReactApp/src/index.tsx
@@ -3,13 +3,27 @@
 // https://www.mackolicious.com/blog/reactjs-into-aspnet-core
 
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+
+// 04/16/2022 11:59 pm - SSN - [20220416-2359] - [001] - Replace ReactDOM.render with  createRoot root.render 
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+// import * as ReactDOM from "react-dom";
+
+import { createRoot } from 'react-dom/client';
+
+
 
 import { App } from "./components/App";
 
-ReactDOM.render(
-	<React.StrictMode>
+const container = document.getElementById("ReactContent")
+
+// 04/16/2022 11:59 pm - SSN - [20220416-2359] - [002] - Replace ReactDOM.render with  createRoot root.render 
+// ReactDOM.render(
+const root = createRoot(container);
+root.render(
+// [20220416-2359] - [002] - End
+
+<React.StrictMode>
 	<App />
-	</React.StrictMode>,
-	document.getElementById("ReactContent")
+	</React.StrictMode>
+	//document.getElementById("ReactContent")
 );
